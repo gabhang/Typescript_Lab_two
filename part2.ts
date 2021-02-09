9319c330d660e48c6510690a189323268a5919ca
LAPTOP-PPOG0DGB <ghfc101@hotmail.com>
2021-02-09 13:46:13 +0800

    Upload lab

---
+++ part2.ts
@@ -0,0 +1,33 @@
+let tasks: string[] = [];
+
+function addTask(task: string): number { 
+    tasks.push(task);
+    console.log("—__________ New Task Added __________");
+    console.log("Task " +task+ " inserted in the list");
+    return tasks.length;
+}
+
+function listAllTasks(): void {
+    console.log("START: All items in Array:");
+    tasks.forEach(function(task){
+        console.log(task);
+    })
+    console.log("END: All items in Array:");
+}
+
+function deleteTask(task: string): number {
+    let index: number = tasks.indexOf(task);
+    if(index > -1) {
+        tasks.splice(index, 1);
+        console.log("__________ Task Removed __________");
+        console.log("Task " +task+ " removed from list.");   
+    }
+    return tasks.length;
+}
+
+addTask('wake up');
+addTask('sleep');
+
+listAllTasks();
+
+deleteTask('sleep');
